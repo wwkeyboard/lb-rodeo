218462bd5357b8f4f2e58397d15dfc6d4b662fe9
Aaron Lee <alee@aaronosaur.us>
2023-03-23 15:57:23 -0500

    three tmux panes

--- tmux-dev-env.sh
+++ tmux-dev-env.sh
@@ -7,12 +7,13 @@ tmux has-session -t lb-dev
 if [ $? != 0 ]
 then
   tmux new-session -s lb-dev -n editor -d
-  tmux send-keys -t lb-dev 'nvim .' C-m
+  #tmux send-keys -t lb-dev 'nvim .' C-m
 
   tmux split-window -v -t lb-dev
-  tmux select-layout -t lb-dev main-horizontal
 
-  tmux split-window -h -t lb-dev:1.2
+  tmux split-window -h -p 66 -t lb-dev:0.1
+  tmux split-window -h -p 50 -t lb-dev:0.2
+
 fi
 tmux attach -t lb-dev
 
